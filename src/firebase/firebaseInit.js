3a95becd56029ff84fd629f035464f80f00e6f62
Mohamed akhelij <m.akhelij@gmail.com>
2021-08-17 16:37:30 +0100

    reCaptcha integration

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -13,7 +13,16 @@ var firebaseConfig = {
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
+firebase.analytics();
+const appCheck = firebase.appCheck();
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+appCheck.activate(
+  '6LfJpAgcAAAAAJdLVgSZXFpnOHpRI8_HgDxKIOLR',
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  true);
 const timestamp = firebase.firestore.FieldValue.serverTimestamp();
 
 export {timestamp};
