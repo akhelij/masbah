01ee42fc681179527160894045aeee62fcd8f86c
makhelij <m.akhelij@gmail.com>
2023-07-06 23:01:30 +0100

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,52 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
+        cleanupOutdatedCaches: false,
+        clientsClaim: true,
+        skipWaiting: true
+      }
+    })
+  ],
   resolve: {
       alias: {
       '@': path.resolve(__dirname, '/src'),
     },
+  },
+  manifest : {
+    name: "Pissina",
+    short_name: "Pissina",
+    description: "1er site de location de piscines entre particulier au Maroc",
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+    icons: [
+      {
+        src: "/icon_x192.png", // Replace with the actual path to your app's icon
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon_x384.png", // Replace with the actual path to your app's icon
+        sizes: "384x384",
+        type: "image/png",
+      },
+      {
+        src: "/icon_x512.png", // Replace with the actual path to your app's icon
+        sizes: "512x512",
+        type: "image/png",
+      },
+      // Add more icons as needed
+    ],
   }
 })
